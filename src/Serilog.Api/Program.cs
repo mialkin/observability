9a3d13d274aabb4b151d785516924e4f3f24dbff
Aleksei Mialkin <aleksei@mialkin.ru>
2025-05-28 11:19:24 +0500

    Add Serilog.Api project

---
+++ src/Serilog.Api/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+    configuration.WriteTo.Console();
+    // configuration.WriteTo.File("/Users/john.doe/Downloads/serilog-api.log");
+});
+
+var services = builder.Services;
+
+services.AddEndpointsApiExplorer();
+
+services.AddRouting(x => x.LowercaseUrls = true);
+
+var application = builder.Build();
+
+application.UseSerilogRequestLogging();
+
+application.UseRouting();
+
+application.MapGet(
+    pattern: "/",
+    handler: ([FromServices] ILogger<Program> logger) =>
+    {
+        logger.LogInformation("Hello");
+        return Results.Ok("Hello");
+    });
+
+application.Run();

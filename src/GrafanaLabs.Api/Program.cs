89f3d69b464f187ca26e7928143ec7d09ae2295f
Aleksei Mialkin <aleksei@mialkin.ru>
2025-08-10 17:46:33 +0500

    Add Serilog

--- src/GrafanaLabs.Api/Program.cs
+++ src/GrafanaLabs.Api/Program.cs
@@ -1,6 +1,17 @@
+using Serilog;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Host.UseSerilog((context, configuration) =>
+{
+    configuration.ReadFrom.Configuration(context.Configuration);
+    configuration.WriteTo.Console();
+});
+
+
 var application = builder.Build();
+
+application.UseSerilogRequestLogging();
 
 application.MapGet("/", () => "GrafanaLabs.Api");
 

14e54f99f0f915776fa8208d2f2405a09dd6a667
Aleksei Mialkin <aleksei@mialkin.ru>
2025-08-17 14:13:11 +0500

    Serilog.Sinks.OpenTelemetry

--- src/GrafanaLabs.Api/Program.cs
+++ src/GrafanaLabs.Api/Program.cs
@@ -1,21 +1,18 @@
 using GrafanaLabs.Api.Telemetry;
 using GrafanaLabs.Api.Telemetry.Meters;
 using Microsoft.AspNetCore.Mvc;
-using OpenTelemetry.Exporter;
-using OpenTelemetry.Logs;
+using Serilog;
+using Serilog.Sinks.OpenTelemetry;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Logging.AddOpenTelemetry(logging =>
+builder.Host.UseSerilog((context, configuration) =>
 {
-    // The rest of your setup code goes here
-    logging.AddOtlpExporter(x =>
-        {
-            x.Endpoint = new Uri("http://localhost:4317");
-            x.Protocol = OtlpExportProtocol.Grpc;
-        })
-        // .AddConsoleExporter()
-        ;
+    configuration.ReadFrom.Configuration(context.Configuration);
+    configuration.WriteTo.Console();
+    configuration.WriteTo.OpenTelemetry(
+        endpoint: "http://localhost:4317",
+        protocol: OtlpProtocol.Grpc);
 });
 
 builder.ConfigureTelemetry();
@@ -25,7 +22,7 @@ services.ConfigureMeters();
 
 var application = builder.Build();
 
-
+application.UseSerilogRequestLogging();
 application.UseOpenTelemetryPrometheusScrapingEndpoint();
 
 application.MapGet("/", () => "GrafanaLabs.Api");
